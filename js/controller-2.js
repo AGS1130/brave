9aa36f06f0de8ae0955fbabd36320c2269892f14
Gregory Salinas <asalinas720@gmail.com>
2019-08-09 21:43:32 -0700

    Restructured Directory

---
+++ js/controller-2.js
@@ -0,0 +1,11 @@
+(function(){
+  'use strict';
+
+  angular.module('ShoppingList', []).controller('ShoppingListController', ShoppingListController);
+
+  ShoppingListController.$inject = ['$scope'];
+
+  function ShoppingListController($scope) {
+
+  }
+})();
---
+++ js/controller-3.js
@@ -0,0 +1,11 @@
+(function () {
+  'use strict';
+
+  angular.module('NarrowDown', []).controller('NarrowDownController', NarrowDownController);
+
+  NarrowDownController.$inject = ['$scope'];
+
+  function NarrowDownController($scope) {
+
+  }
+})();
