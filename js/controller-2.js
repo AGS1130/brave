23fecf7b5cceec2164153588de323317f9fbe2a7
Gregory Salinas <asalinas720@gmail.com>
2019-08-10 18:02:07 -0700

    Functionality Set with Controllers in Page 2

--- js/controller-2.js
+++ js/controller-2.js
@@ -1,11 +1,64 @@
-(function(){
+(function () {
   'use strict';
 
-  angular.module('ShoppingList', []).controller('ShoppingListController', ShoppingListController);
+  angular.module('ShoppingListCheckOff', [])
+    .controller('ToBuyController', ToBuyController)
+    .controller('AlreadyBoughtController', AlreadyBoughtController)
+    .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-  ShoppingListController.$inject = ['$scope'];
+  ToBuyController.$inject = ['ShoppingListCheckOffService'];
+  function ToBuyController(ShoppingListCheckOffService) {
+    var thisController = this;
+    var shoppingList = ['cookie', 'chip', 'soda', 'apple', 'bannana', 'pear'];
 
-  function ShoppingListController($scope) {
+    //
+    thisController.items = ShoppingListCheckOffService.randomList(shoppingList);
+    thisController.boughtItem = function (itemIndex) {
+      ShoppingListCheckOffService.boughtItem(itemIndex);
+    }
+  }
 
+  AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+  function AlreadyBoughtController(ShoppingListCheckOffService) {
+    var thisController = this;
+
+    thisController.items = ShoppingListCheckOffService.getBoughtItems();
+  }
+
+  function ShoppingListCheckOffService() {
+    var service = this;
+
+    // List of items
+    var items = [];
+    var itemsBought = [];
+
+    service.randomList = function (shoppingList) {
+      var listLength = shoppingList.length;
+
+      for (var i = 0; i < listLength; i++) {
+        var item = {};
+        var randomIndex = Math.floor(Math.random() * shoppingList.length);
+
+        // Initalize item object with values and push to items array
+        item.quantity = Math.floor(Math.random() * 10) + 1;
+        item.name = item.quantity === 1 ? shoppingList[randomIndex] : shoppingList[randomIndex] + 's';
+        items.push(item);
+
+        // Remove duplicate values
+        shoppingList.splice(randomIndex, 1);
+        listLength -= 1;
+      }
+
+      return items
+    }
+
+    service.boughtItem = function (itemIndex) {
+      itemsBought.push(items[itemIndex]);
+      items.splice(itemIndex, 1);
+    }
+
+    service.getBoughtItems = function() {
+      return itemsBought;
+    }
   }
 })();
